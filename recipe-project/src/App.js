3ac95e67a2974dedd9dc36e7ae38a46052ef4523
Jsparks1989 <jsparks19891@gmail.com>
2019-11-12 00:34:33 -0500

    Set the chicken hits data to the state recipes variable. Now the chicken recipes is state data for App.

--- recipe-project/src/App.js
+++ recipe-project/src/App.js
@@ -3,25 +3,21 @@ import './App.css';
 
 const App = () => {
 
-  const APP_ID  = '59b86a22';
-  const APP_KEY = '01bb75cd1763508e37d3be89a8b4d4ce';
 
-  // The state
+
+
   const [recipes, setRecipes] = useState([]);
 
-  // useEffect() runs when the page is re-rendered. The second param (the empty array), will make useEffect() only run once.
-  // getRecipes() will be ran when useEffect() is ran.
   useEffect(() =>{
     getRecipes();
   }, []);
 
 
-  // getRecipes is the function used to make a connection to the edamam API. Using 'await' bc you dont know when the data
-  // will be returned. Setting the data returned to JSON format so its easier to read.
   const getRecipes = async () =>{
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data.hits);
+    // Now all chicken recipes are in the recipes state.
+    setRecipes(data.hits);
   }
 
 
