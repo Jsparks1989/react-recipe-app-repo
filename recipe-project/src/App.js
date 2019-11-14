b8bc5e52d00ade6d7c98777c889be8b7703c930d
Jsparks1989 <jsparks19891@gmail.com>
2019-11-14 15:11:40 -0500

    Added/changed some comments to help follow along.

--- recipe-project/src/App.js
+++ recipe-project/src/App.js
@@ -5,37 +5,40 @@ import Recipe from './Recipe';
 
 
 const App = () => {
-
+  //-- Edamam API data
   const APP_ID  = '59b86a22';
   const APP_KEY = '01bb75cd1763508e37d3be89a8b4d4ce';
 
-  
+
+  //-- State -> "[state variable, function for setting the state variable] = useState(default value of the state variable)"
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('chicken');
 
 
+  //-- Everytime the page is re-rendered, the API call is made again.
   useEffect(() =>{
     getRecipes();
   }, []);
 
 
+  //-- Making the API call for Edamam recipes and setting the fetched data from the call to json format.
   const getRecipes = async () =>{
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
   }
 
-
+  // Setting what the user typed in the search-bar (e.target.value) to the state.search. 
   const updateSearch = (e) =>{
     setSearch(e.target.value);
     console.log(search);
   }
 
 
-// When search-form is submitted, run this function.
+// When search-form is submitted, run this function. Has an event passed in as a parameter.
   const getSearch = (e) => {
-    // Want to stop the page refresh
+    // This will stop the browser re-loading when form is submitted.
     e.preventDefault();
     // Setting query to what is in the search-bar
     setQuery(search);
@@ -44,7 +47,6 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* Run getSearch when the form is submitted. */}
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type='text' value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
